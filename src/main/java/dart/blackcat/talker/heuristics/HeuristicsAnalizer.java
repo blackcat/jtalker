4f8f167468b1a7dfbed81e190f24524cf92a6f48
Pavel Vyazankin <pavel.vyazankin@gmail.com>
2009-09-12 15:21:05 +0400

    ingnore maven target/*

---
+++ src/main/java/dart/blackcat/talker/heuristics/HeuristicsAnalizer.java
@@ -0,0 +1,17 @@
+package dart.blackcat.talker.heuristics;
+
+import java.util.Set;
+
+import dart.blackcat.talker.JTalkerException;
+import dart.blackcat.talker.domain.MorphologyAnalysis;
+import dart.blackcat.talker.domain.MorphologyAnalyzer;
+
+public class HeuristicsAnalizer implements MorphologyAnalyzer {
+
+	@Override
+	public Set<MorphologyAnalysis> analyze(String word) throws JTalkerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
